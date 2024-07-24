abba287ae8f040752fab675ad19b5313314b32fe
Ahsan Javed <jahsan850@gmail.com>
2024-07-24 22:42:26 +0500

    validate-changes

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,8 +3,8 @@ export const  checkValidData = (email : string, password : string, name: string)
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
 
-    if(!isEmailValid) return "Email is not valid"
-    if(!isPasswordValid) return "Password is not valid"
+    if(!isEmailValid) return "Email must contain @ and .com"
+    if(!isPasswordValid) return "Password must contains Capital letter, numeric and symbols"
     if(name) console.log(name)
 
     return null;
